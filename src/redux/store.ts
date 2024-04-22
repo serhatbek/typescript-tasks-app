8b12203f81fb023f627caaf4b19f994caa1683c2
serhatbek <serhat.bekk@gmail.com>
2024-04-22 14:37:40 +0300

    redux custom hooks

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import taskReducer from './Tasks/tasksSlice';
 
-const store = configureStore({
+export const store = configureStore({
   reducer: {
     Tasks: taskReducer,
   },
